99faa13283f75aa18d70e2fe5149886960d2353d
Roel van Duijnhoven <roel@vanduijnhoven.com>
2025-11-21 09:16:17 +0100

    Enhance AlbumListController to support dynamic sorting of album list items by album title, artist name, or position based on query parameters. Update show.html.twig to include sortable headers for each column, reflecting the current sort direction. Minor fix in dashboard template for list formatting.

--- src/Controller/AlbumListController.php
+++ src/Controller/AlbumListController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use App\Entity\AlbumList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AlbumListController extends AbstractController
 {
     #[Route('/list/{id}', name: 'app_list_show')]
-    public function show(AlbumList $albumList): Response
+    public function show(AlbumList $albumList, Request $request): Response
     {
         // Get items and sort based on list type
         $items = $albumList->getListItems()->toArray();
@@ -23,24 +24,53 @@ class AlbumListController extends AbstractController
                 break;
             }
         }
+
+        $sort = $request->query->get('sort');
+        $direction = $request->query->get('direction', 'asc');
         
         // Sort items
-        if ($hasMentions) {
-            // Sort by mentions descending (highest first)
+        if ($sort === 'album') {
+            usort($items, function($a, $b) use ($direction) {
+                $valA = $a->getAlbum()->getTitle();
+                $valB = $b->getAlbum()->getTitle();
+                return $direction === 'asc' ? strcasecmp($valA, $valB) : strcasecmp($valB, $valA);
+            });
+        } elseif ($sort === 'artist') {
+            usort($items, function($a, $b) use ($direction) {
+                $valA = $a->getAlbum()->getArtist()->getName();
+                $valB = $b->getAlbum()->getArtist()->getName();
+                return $direction === 'asc' ? strcasecmp($valA, $valB) : strcasecmp($valB, $valA);
+            });
+        } elseif ($sort === 'position') {
+            if ($hasMentions) {
+                usort($items, function($a, $b) use ($direction) {
+                    $mentionsA = $a->getMentions() ?? 0;
+                    $mentionsB = $b->getMentions() ?? 0;
+                    return $direction === 'asc' ? $mentionsA <=> $mentionsB : $mentionsB <=> $mentionsA;
+                });
+            } else {
+                usort($items, function($a, $b) use ($direction) {
+                    $posA = $a->getPosition() ?? PHP_INT_MAX;
+                    $posB = $b->getPosition() ?? PHP_INT_MAX;
+                    return $direction === 'asc' ? $posA <=> $posB : $posB <=> $posA;
+                });
+            }
+        } elseif ($hasMentions) {
+            // Default sort by mentions descending (highest first)
             usort($items, function($a, $b) {
                 $mentionsA = $a->getMentions() ?? 0;
                 $mentionsB = $b->getMentions() ?? 0;
                 return $mentionsB <=> $mentionsA;
             });
         } elseif ($albumList->getType() === AlbumList::TYPE_ORDERED) {
-            // Sort by position ascending (lowest first)
+            // Default sort by position ascending (lowest first)
             usort($items, function($a, $b) {
                 $posA = $a->getPosition() ?? PHP_INT_MAX;
                 $posB = $b->getPosition() ?? PHP_INT_MAX;
                 return $posA <=> $posB;
             });
         }
-        // TYPE_UNORDERED and TYPE_AGGREGATE lists: no sorting needed
+        // TYPE_UNORDERED and TYPE_AGGREGATE lists: no sorting needed by default
         
         $computedItems = [];
         if ($albumList->getType() === AlbumList::TYPE_AGGREGATE) {
@@ -63,13 +93,35 @@ class AlbumListController extends AbstractController
                 }
             }
 
-            arsort($scores);
-
             foreach ($scores as $albumId => $score) {
                 $computedItems[] = [
                     'album' => $albums[$albumId],
                     'score' => $score,
                 ];
+            }
+
+            // Sort computed items
+            if ($sort === 'album') {
+                usort($computedItems, function($a, $b) use ($direction) {
+                    $valA = $a['album']->getTitle();
+                    $valB = $b['album']->getTitle();
+                    return $direction === 'asc' ? strcasecmp($valA, $valB) : strcasecmp($valB, $valA);
+                });
+            } elseif ($sort === 'artist') {
+                usort($computedItems, function($a, $b) use ($direction) {
+                    $valA = $a['album']->getArtist()->getName();
+                    $valB = $b['album']->getArtist()->getName();
+                    return $direction === 'asc' ? strcasecmp($valA, $valB) : strcasecmp($valB, $valA);
+                });
+            } elseif ($sort === 'position') {
+                usort($computedItems, function($a, $b) use ($direction) {
+                    return $direction === 'asc' ? $a['score'] <=> $b['score'] : $b['score'] <=> $a['score'];
+                });
+            } else {
+                // Default sort by score descending
+                usort($computedItems, function($a, $b) {
+                    return $b['score'] <=> $a['score'];
+                });
             }
         }
 
@@ -78,6 +130,8 @@ class AlbumListController extends AbstractController
             'items' => $items,
             'hasMentions' => $hasMentions,
             'computedItems' => $computedItems,
+            'currentSort' => $sort,
+            'currentDirection' => $direction,
         ]);
     }
 }
